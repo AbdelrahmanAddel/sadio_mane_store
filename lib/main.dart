3fe2d0dbe7a6db2dd32f680561cc9dd33da64210
Abdelrahman <Aa5282243@gmail.com>
2025-06-19 15:42:49 +0300

    feat: add environment configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sadio_mane_store/core/app/env_variable.dart';
+import 'package:sadio_mane_store/sadio_mane_app.dart';
 
-void main(){
-  runApp(const MyApp());
-}
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() async {
+  await EnvVariable.getInstance.loadEnv(envType: EnvType.dev);
 
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then((_) {
+    runApp(const SadioManeApp());
+  });
 }
