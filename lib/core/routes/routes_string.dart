cfe6bd753f1cfeb3236de872dafad8283ce65151
Abdelrahman <Aa5282243@gmail.com>
2025-06-20 18:18:48 +0300

    feat: Implement navigation and routing

---
+++ lib/core/routes/routes_string.dart
@@ -0,0 +1,6 @@
+class RoutesString {
+  RoutesString._();
+  static const String noInternetConnectionScreen =
+      'no_internet_connection_screen';
+  static const String homeScreen = 'home_screen';
+}
