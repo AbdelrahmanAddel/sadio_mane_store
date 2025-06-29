07e21174084993821867162e173fb0533f9769e7
Abdelrahman <Aa5282243@gmail.com>
2025-06-30 00:39:18 +0300

    feat: Add dashboard view and related widgets
    - Implemented CustomContainerLinearAdmin for styled containers.
    - Created DashBoardView with a custom app bar and refresh functionality.
    - Developed DashBoardBody to display product, category, and user statistics.
    - Added DashBoardContinerContent for structured content display within containers.
    - Introduced LoadingShimmer for loading indicators.
    - Created LoadingDashBoardScreen to show loading states for dashboard items.

--- lib/features/dashboard/presentation/view/dashboard_view.dart
+++ lib/features/dashboard/presentation/view/dashboard_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sadio_mane_store/core/common/widget/custom_admin_app_bar.dart';
 import 'package:sadio_mane_store/core/theme/extensions/app_theme_extension.dart';
+import 'package:sadio_mane_store/features/dashboard/presentation/widgets/dashboard_body.dart';
 
 class DashBoardView extends StatelessWidget {
   const DashBoardView({super.key});
@@ -13,6 +14,13 @@ class DashBoardView extends StatelessWidget {
         backGrounfColor: context.theme.appColors.mainColor,
         title: 'DashBoard',
         isMain: true,
+      ),
+      body: SingleChildScrollView(
+        child: RefreshIndicator(
+          onRefresh: () async {},
+
+          child: const DashBoardBody(),
+        ),
       ),
     );
   }
