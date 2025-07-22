19ba69a48a571f6df83ac8c0fc30bb456e2b91a0
Abdelrahman <Aa5282243@gmail.com>
2025-07-22 10:55:04 +0300

    Fix notification refresh bug

--- lib/features/notification/presentation/widget/notification_view/notification_list_view.dart
+++ lib/features/notification/presentation/widget/notification_view/notification_list_view.dart
@@ -3,7 +3,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sadio_mane_store/core/common/widget/custom_container_linear_admin.dart';
 import 'package:sadio_mane_store/core/helpers/spacer_helper.dart';
+import 'package:sadio_mane_store/features/notification/data/model/notification_content_model.dart';
 import 'package:sadio_mane_store/features/notification/presentation/bloc/notification_bloc.dart';
+import 'package:sadio_mane_store/features/notification/presentation/bloc/notification_event.dart';
 import 'package:sadio_mane_store/features/notification/presentation/bloc/notification_state.dart';
 import 'package:sadio_mane_store/features/notification/presentation/widget/notification_view/notification_continer_content.dart';
 
@@ -28,30 +30,48 @@ class NotificationListView extends StatelessWidget {
           GetNotificationLoadingState() => const Center(
             child: CircularProgressIndicator(),
           ),
-          GetNotificationSuccessState() => ListView.separated(
-            reverse: true,
-            physics: const BouncingScrollPhysics(),
-            shrinkWrap: true,
-            itemCount: state.notificationContentModels.length,
-            itemBuilder: (context, index) {
-              return CustomContainerLinearAdmin(
-                height: 200.h,
-                width: double.infinity,
-                child: NotificationContinerContent(
-                  currentIndex: index,
-                  notificationContentModel:
-                      state.notificationContentModels[index],
-                ),
-              );
-            },
-            separatorBuilder: (BuildContext context, int index) {
-              return verticalSpace(20);
-            },
+          GetNotificationSuccessState() => GetNotificationSuccessStateWidget(
+            notificationContentModels: state.notificationContentModels,
           ),
           GetNotificationErrorState() => Center(child: Text(state.message)),
           _ => const SizedBox.shrink(),
         };
       },
+    );
+  }
+}
+
+class GetNotificationSuccessStateWidget extends StatelessWidget {
+  const GetNotificationSuccessStateWidget({
+    required this.notificationContentModels,
+    super.key,
+  });
+  final List<NotificationContentModel> notificationContentModels;
+
+  @override
+  Widget build(BuildContext context) {
+    return RefreshIndicator(
+      onRefresh: () async {
+        context.read<NotificationBloc>().add(GetNotificationEvent());
+      },
+      child: ListView.separated(
+        reverse: true,
+        shrinkWrap: true,
+        itemCount: notificationContentModels.length,
+        itemBuilder: (context, index) {
+          return CustomContainerLinearAdmin(
+            height: 200.h,
+            width: double.infinity,
+            child: NotificationContinerContent(
+              currentIndex: index,
+              notificationContentModel: notificationContentModels[index],
+            ),
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) {
+          return verticalSpace(20);
+        },
+      ),
     );
   }
 }
--- lib/features/notification/presentation/widget/notification_view/notification_view_body.dart
+++ lib/features/notification/presentation/widget/notification_view/notification_view_body.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sadio_mane_store/core/helpers/spacer_helper.dart';
-import 'package:sadio_mane_store/features/notification/presentation/bloc/notification_bloc.dart';
-import 'package:sadio_mane_store/features/notification/presentation/bloc/notification_event.dart';
 import 'package:sadio_mane_store/features/notification/presentation/widget/notification_add/add_notification_header.dart';
 import 'package:sadio_mane_store/features/notification/presentation/widget/notification_view/notification_list_view.dart';
 
@@ -14,19 +11,14 @@ class NotificationViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 12.w),
-      child: RefreshIndicator(
-        onRefresh: () async {
-          context.read<NotificationBloc>().add(GetNotificationEvent());
-        },
-        child: CustomScrollView(
-          physics: const BouncingScrollPhysics(),
-          slivers: [
-            SliverToBoxAdapter(child: verticalSpace(30)),
-            const SliverToBoxAdapter(child: AddNotificationHeader()),
-            SliverToBoxAdapter(child: verticalSpace(30)),
-            const SliverToBoxAdapter(child: NotificationListView()),
-          ],
-        ),
+      child: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
+        slivers: [
+          SliverToBoxAdapter(child: verticalSpace(30)),
+          const SliverToBoxAdapter(child: AddNotificationHeader()),
+          SliverToBoxAdapter(child: verticalSpace(30)),
+          const SliverToBoxAdapter(child: NotificationListView()),
+        ],
       ),
     );
   }
